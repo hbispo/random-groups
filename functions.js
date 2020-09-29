5e708edb6d546ee5870a89939b884f815c746d7f
Henrique Bispo <henrique@obispo.com.br>
2020-09-29 00:37:18 -0300

    Capitalize only first letter of each name

--- functions.js
+++ functions.js
@@ -90,7 +90,8 @@ function pickGroups(max, min = 0) {
     window['currentGroups'] = [];
     // Splitting the lines, trimming them, removing non-letters from the beginning of each one and removing the empty ones
     let people = groupsForm.people.value.split("\n").map(function (word) {
-        return word.trim().replace(/^[\d]*[\W]*/, '').trim();
+        word = word.trim().replace(/^[\d]*[\W]*/, '').trim().toLocaleLowerCase();
+        return word[0].toLocaleUpperCase() + word.substr(1);
     }).filter(function (word) {
         return word.length > 0;
     });
